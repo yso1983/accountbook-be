3fae41bd2f66ab192b89cc6b308cc177c3a386b2
yso1983 <yso1983.gmail.com>
2021-12-10 00:57:31 +0900

    dnw

--- app/controllers/accountbook/dnw.controller.js
+++ app/controllers/accountbook/dnw.controller.js
@@ -74,9 +74,20 @@ exports.createDetail = (req, res) => {
     created_user_id: req.body.user_id, 
     amount: commFunc.parseFloat(req.body.amount), 
     standard_dt: req.body.standard_dt, 
-    remark: req.body.remark  
+    remark: req.body.remark,
   };
-  
+
+  const to_account_id = req.body.to_account_id;
+  if(to_account_id && commFunc.parseFloat(to_account_id) > 0){
+    createDetailFromAndTo(params, to_account_id, res) ;
+  }
+  else{
+    createOnlyDetail(params, res);
+  }
+
+};
+
+function createOnlyDetail(params, res) {
   Account.findOne({
     where: {
       id: params.account_id
@@ -102,7 +113,12 @@ exports.createDetail = (req, res) => {
 
           return DnwDetail.create(params, {transaction: t}); 
         })
+        .then(result => {
+
+
+        })
         .then((result) => { 
+
           t.commit(); //커밋 
 
           return res.status(200).send(success(result));
@@ -115,12 +131,80 @@ exports.createDetail = (req, res) => {
     })
     .catch(err => res.status(401).send(failure("9999", err.message)));
   });
-  // upsert(DnwDetail, params, { id: req.body.id })
-  //   .then(result => {
-  //      res.status(200).send(success(result));
-  //   })
-  //   .catch(err => res.status(401).send(failure("9999", err.message)));
-};
+}
+
+
+function createDetailFromAndTo(params, to_account_id, res) {
+
+
+  Account.findOne({
+    where: {
+      id: params.account_id
+    }
+  })
+  .then(account => {
+
+    let accountParams = {
+      id : account.id,
+      amount : (parseFloat(account.amount) + parseFloat(params.amount)),
+      user_id : account.user_id,
+      name : account.name,
+      remark : account.remark
+    };
+
+    Account.findOne({
+      where: {
+        id: to_account_id
+      }
+    })
+    .then(toAccount => {
+
+      let toAccountParams = {
+        id : toAccount.id,
+        amount : (parseFloat(toAccount.amount) - parseFloat(params.amount)),
+        user_id : toAccount.user_id,
+        name : toAccount.name,
+        remark : toAccount.remark
+      };
+
+      db.sequelize.transaction()
+      .then(t => { 
+        console.log(accountParams);
+        
+        return account.update(accountParams, { transaction: t })
+          .then(result => { 
+            console.log(params);
+
+            return DnwDetail.create(params, {transaction: t}); 
+          })
+          .then(result => {
+            
+            return toAccount.update(toAccountParams, { transaction: t });
+          })
+          .then(result => { 
+            params.account_id = to_account_id;
+            params.amount = 0 - params.amount;
+
+            return DnwDetail.create(params, {transaction: t}); 
+          })
+          .then((result) => { 
+
+            t.commit(); //커밋 
+
+            return res.status(200).send(success(result));
+          }).catch((err) => { 
+            console.log(err.message);
+            t.rollback(); //롤백 설정 
+            return res.status(401).send(failure("9998", err.message));
+          }); 
+
+      })
+      .catch(err => res.status(401).send(failure("9997", err.message)));
+
+    })
+    .catch(err => res.status(401).send(failure("9999", err.message)));
+  });
+}
 
 exports.findDetailsByMonth = (req, res) => {
   let startDt = moment(new Date(req.params.month.replace('-', '').substring(0, 4), (parseInt(req.params.month.replace('-', '').substring(4)) - 1), 1, 0, 0, 0))
